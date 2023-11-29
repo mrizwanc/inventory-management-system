d969e4bc57238bd63cfd3f66fd05d4ece2183977
mrizwanc <mrizwanc6@gmail.com>
2023-11-29 21:38:34 +0600

    Initial

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    
+    /**
+     * Show the profile for a given user.
+     */
+    public function show(): View
+    {
+         return 'Hello world';
+    }
+}
